578b02f97734fb2687ea236d9f878f968acd2d4f
cassienoelle <cassienoelle@gmail.com>
2019-10-23 15:48:44 -0400

    fixed responsive footer with javascript

--- site/js/script.js
+++ site/js/script.js
@@ -1,1 +1,33 @@
 "use strict";
+
+
+let $content;
+let $footer;
+let $offsetHeight;
+let $footerHeight;
+
+$(document).ready(function() {
+
+  $content = $('.main-wrapper');
+  $footer = $('.main-foot');
+
+  setFooterHeight();
+
+});
+
+$(window).resize(()=> {
+  setFooterHeight();
+});
+
+function setFooterHeight() {
+
+  if ($content.height() < $(window).height()) {
+    $footerHeight = $footer.height();
+    $offsetHeight = $footerHeight + $(window).height() - $content.height();
+    $footer.css('height', $offsetHeight + 'px');
+  }
+  else if ($content.height() >= $(window).height()) {
+    $footer.css('height', 'auto');
+  }
+
+}
