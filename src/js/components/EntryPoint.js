29451879a0809d1b9b8eeafa115585ae546e887e
Alex <26582461+aheggie@users.noreply.github.com>
2020-09-05 05:45:37 -0400

    route working for all dynamic PaGEs

--- src/js/components/EntryPoint.js
+++ src/js/components/EntryPoint.js
@@ -6,6 +6,8 @@ import OfferingPage from "./OfferingPage";
 import WhyPage from "./WhyPage";
 import AboutPage from "./AboutPage";
 import ResourcesPage from "./ResourcesPage";
+import FilterPage from "./FilterPage";
+import BioPage from "./BioPage";
 
 const EntryPoint = () => {
   return (
@@ -16,6 +18,10 @@ const EntryPoint = () => {
       <Route exact path="/why" component={WhyPage} />
       <Route exact path="/about" component={AboutPage} />
       <Route exact path="/resources" component={ResourcesPage} />
+      {/* these are the dynamic routes for now */}
+      {/* need to figure out how to fail effectively */}
+      <Route exact path="/filter-by/:providerType" component={FilterPage} />
+      <Route exact path="practitioner/:practitionerId" component={BioPage} />
     </Switch>
   );
 };
