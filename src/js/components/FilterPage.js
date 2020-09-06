b3ea831c90f070df56f4deeeb6c0379274bf8b71
Alex <26582461+aheggie@users.noreply.github.com>
2020-09-05 22:01:54 -0400

    Added Quik prop check to FilterPage

--- src/js/components/FilterPage.js
+++ src/js/components/FilterPage.js
@@ -1,7 +1,9 @@
 import React from "react";
 import HeaderFooter from "./HeaderFooter";
 
-const FilterPage = () => {
+const FilterPage = ({ match }) => {
+  //this works!
+  alert(match.params.providerType);
   return <HeaderFooter className="services">Filters</HeaderFooter>;
 };
 
