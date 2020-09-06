814817116565fa258afe9e04d0c4c8bfe14e8fce
Alex <26582461+aheggie@users.noreply.github.com>
2020-09-06 01:41:26 -0400

    added dynamic page title updating to FilterPage

--- src/js/components/FilterPage.js
+++ src/js/components/FilterPage.js
@@ -2,10 +2,18 @@ import React from "react";
 import HeaderFooter from "./HeaderFooter";
 import { appendTitle } from "../utilities";
 
+//providerType is of format "type-of-profession"
+//for page title we want "Type Of Profession"
+const titleCase = (providerType) =>
+  providerType
+    .split("-")
+    .map((word) => `${word[0].toUpperCase()}${word.slice(1)}`)
+    .join(" ");
+
 const FilterPage = ({ match }) => {
   //this works!
-  alert(match.params.providerType);
-  appendTitle();
+  const { providerType } = match.params;
+  appendTitle(titleCase(providerType));
   return <HeaderFooter className="services">Filters</HeaderFooter>;
 };
 
