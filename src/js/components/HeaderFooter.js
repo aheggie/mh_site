f2b8670cbedf64395061ff852e5fa7b3c38a01da
Alex <26582461+aheggie@users.noreply.github.com>
2020-09-09 03:00:20 -0400

    All windows start at top now!

--- src/js/components/HeaderFooter.js
+++ src/js/components/HeaderFooter.js
@@ -3,6 +3,12 @@ import Nav from "./Nav";
 import Footer from "./Footer";
 
 const HeaderFooter = ({ className, children }) => {
+  //because we are not actually loading new .html pages when sent
+  //to new routes by <Link /> (from React Router DOM)
+  //the page stays scrolled to its previous scroll point
+  //
+  //this restores expected behaviour of a page starting at the top
+  window.scrollTo(0, 0);
   return (
     <div className={`wrapper ${className} main-wrapper`}>
       <Nav />
