bf152ed0ed4d60c35a64dcf00345a38bf66c4907
Alex <26582461+aheggie@users.noreply.github.com>
2020-09-09 16:55:32 -0400

    Sidebar broken into sub-components within same file

--- src/js/components/PractitionerPage/BioSidebar.js
+++ src/js/components/PractitionerPage/BioSidebar.js
@@ -1,64 +1,52 @@
 import React from "react";
 
+const placeholderItems = [
+  "Lorem ipsum dolor sit amet, consectetur.",
+  "Lorem ipsum dolor sit amet, consectetur.",
+  "Lorem ipsum dolor sit amet, consectetur.",
+  "Lorem ipsum dolor sit amet, consectetur.",
+];
+
+const BioSideBarBox = ({ className, title, items }) => {
+  return (
+    <div className={`bio ${className}`}>
+      <h3>{title}</h3>
+      <div className="bio-box sidebar-box">
+        <ul className="bio-sidebar-list">
+          {items.map((item, i) => (
+            <li key={i}>{item}</li>
+          ))}
+        </ul>
+        <div className="expand">
+          <span className="see-more">
+            <a href="" className="clear-style">
+              SEE MORE
+            </a>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const BioSidebar = () => {
   return (
     <div className="bio-sidebar">
-      <div className="bio specializations">
-        <h3>Specializations</h3>
-        <div className="bio-box sidebar-box">
-          <ul className="bio-sidebar-list">
-            <li>Lorem ipsum dolor sit amet, consectetur.</li>
-            <li>Lorem ipsum dolor sit amet, consectetur.</li>
-            <li>Lorem ipsum dolor sit amet, consectetur.</li>
-            <li>Lorem ipsum dolor sit amet, consectetur.</li>
-          </ul>
-          <div className="expand">
-            <span className="see-more">
-              <a href="" className="clear-style">
-                SEE MORE
-              </a>
-            </span>
-          </div>
-        </div>
-      </div>
-
-      <div className="bio issues">
-        <h3>Issues I can support you with</h3>
-        <div className="bio-box sidebar-box">
-          <ul className="bio-sidebar-list">
-            <li>Lorem ipsum dolor sit amet, consectetur.</li>
-            <li>Lorem ipsum dolor sit amet, consectetur.</li>
-            <li>Lorem ipsum dolor sit amet, consectetur.</li>
-            <li>Lorem ipsum dolor sit amet, consectetur.</li>
-          </ul>
-          <div className="expand">
-            <span className="see-more">
-              <a href="" className="clear-style">
-                SEE MORE
-              </a>
-            </span>
-          </div>
-        </div>
-      </div>
-
-      <div className="bio approach">
-        <h3>Helping approach</h3>
-        <div className="bio-box sidebar-box">
-          <ul className="bio-sidebar-list">
-            <li>Lorem ipsum dolor sit amet, consectetur.</li>
-            <li>Lorem ipsum dolor sit amet, consectetur.</li>
-            <li>Lorem ipsum dolor sit amet, consectetur.</li>
-            <li>Lorem ipsum dolor sit amet, consectetur.</li>
-          </ul>
-          <div className="expand">
-            <span className="see-more">
-              <a href="" className="clear-style">
-                SEE MORE
-              </a>
-            </span>
-          </div>
-        </div>
-      </div>
+      <BioSideBarBox
+        className="specializations"
+        title="Specializations"
+        items={placeholderItems}
+      />
+      <BioSideBarBox
+        className="issues"
+        title="Issues I can support you with"
+        items={placeholderItems}
+      />
+      <BioSideBarBox
+        className="approach"
+        title="Helping approach"
+        items={placeholderItems}
+      />
     </div>
   );
 };
