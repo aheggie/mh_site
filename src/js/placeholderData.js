cfcc7cadc6d9a133a50bb8b2ea1b20b1ff20fcc5
Alex <26582461+aheggie@users.noreply.github.com>
2020-09-11 02:26:14 -0400

    updated placeholderData to be more variable in what it sends

--- src/js/placeholderData.js
+++ src/js/placeholderData.js
@@ -7,14 +7,13 @@ const placeholderItems = [
   "Lorem ipsum dolor sit amet, consectetur.",
 ];
 
-const placeholderData = [
-  {
-    id: 1,
-    name: "Dr. Jane Doe",
-    title: "Psychologist, PhD, OPQ",
-    photo: placeholderPhoto,
-    //note: this is probably the most divergent from what firebase db will send?
-    aboutText: `<p>
+const placeholderPractitionerOne = {
+  id: 1,
+  name: "Dr. Jane Doe",
+  title: "Psychologist, PhD, OPQ",
+  photo: placeholderPhoto,
+  //note: this is probably the most divergent from what firebase db will send?
+  aboutText: `<p>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id velit
     faucibus, pharetra mauris non, dictum nunc. Vestibulum sem lorem, semper
     sed iaculis at, ultricies non lacus. Suspendisse vestibulum tellus quis
@@ -42,36 +41,39 @@ const placeholderData = [
     ornare at consectetur eget, commodo in sapien. Nullam vulputate laoreet
     consequat.
   </p>`,
-    email: "janedoe@psychologist.ca",
-    phone: "514-000-0000",
-    address: "1455 de Maisonneuve Blvd West H3G 1M8",
-    //also not sure how this will actually load in from db
-    languages: ["English", "French", "Haitian Créole"],
-    // these hydrate the sidebar in PractitionerPage
-    specialisationsItems: placeholderItems,
-    issuesItems: placeholderItems,
-    approachItems: placeholderItems,
-    //these hydrate the BioPayment component
-    paymentRates: {
-      individual: 100,
-      studentIndividual: 75,
-      coupleFamily: 125,
-      studentCoupleFamily: 100,
-    },
-    paymentMethods: ["Cash", "etransfer", "cheque"],
-    insuranceAccepted: ["PlanGreen", "PlanYellow"],
-    accessibilityBooleans: {
-      wheelchair: true,
-      trans: true,
-      slidingScale: true,
-    },
-    orderAffiliation: "Order of Psychologists of Quebec (OPQ)",
-    licenseNumber: "1280058",
-    schooling: "McGill University Post-Doc - Clinical Psychology",
-    yearGraduated: "2015",
-    yearsInPractice: 5,
-    extraCredentials: `Phasellus sagittis, risus id gravida feugiat.`,
+  email: "janedoe@psychologist.ca",
+  phone: "514-000-0000",
+  address: "1455 de Maisonneuve Blvd West H3G 1M8",
+  //also not sure how this will actually load in from db
+  languages: ["English", "French", "Haitian Créole"],
+  // these hydrate the sidebar in PractitionerPage
+  specialisationsItems: placeholderItems,
+  issuesItems: placeholderItems,
+  approachItems: placeholderItems,
+  //these hydrate the BioPayment component
+  paymentRates: {
+    individual: 100,
+    studentIndividual: 75,
+    coupleFamily: 125,
+    studentCoupleFamily: 100,
   },
-];
+  paymentMethods: ["Cash", "etransfer", "cheque"],
+  insuranceAccepted: ["PlanGreen", "PlanYellow"],
+  accessibilityBooleans: {
+    wheelchair: true,
+    trans: true,
+    slidingScale: true,
+  },
+  orderAffiliation: "Order of Psychologists of Quebec (OPQ)",
+  licenseNumber: "1280058",
+  schooling: "McGill University Post-Doc - Clinical Psychology",
+  yearGraduated: "2015",
+  yearsInPractice: 5,
+  extraCredentials: `Phasellus sagittis, risus id gravida feugiat.`,
+};
+
+const placeholderData = Array.from(new Array(9)).map(
+  () => placeholderPractitionerOne
+);
 
 export default placeholderData;
