70f27588571d893255fb613ab118445886140023
Alex <26582461+aheggie@users.noreply.github.com>
2020-09-12 05:22:46 -0400

    languageSwitch object implemented in Footer component

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -2,6 +2,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import routes from "../routes";
+import { curriedFillInStaticTextByLang, enAndFrContent } from "../utilities";
+
+const staticText = {
+  EmailSignUpText: enAndFrContent(
+    "Sign up for email updates",
+    "Sign up for email updates"
+  ),
+  SeekingText: enAndFrContent("Seeking Services", "Seeking Services"),
+  OfferingText: enAndFrContent("Offering Services", "Offering Services"),
+  WhyText: enAndFrContent("Why Does This Exist?", "Why Does This Exist?"),
+  AboutText: enAndFrContent("About Us", "About Us"),
+  ResourcesText: enAndFrContent("Resources", "Resources"),
+  CopyrightText: enAndFrContent(
+    "Copyright © CURE Concordia 2019, Inc. All rights reserved.",
+    "Copyright © CURE Concordia 2019, Inc. All rights reserved."
+  ),
+};
 
 const FooterNavLink = ({ to, linkText }) => {
   return (
@@ -12,13 +29,14 @@ const FooterNavLink = ({ to, linkText }) => {
 };
 
 const Footer = ({ lang }) => {
+  const fillText = curriedFillInStaticTextByLang(lang);
   return (
     <footer className="main-foot">
       <div className="wrapper footer">
         <ul className="footer-social">
           <li>
             <a href="" className="clear-style" id="email">
-              Sign up for email updates
+              {fillText(staticText.EmailSignUpText)}
             </a>
           </li>
           <li>
@@ -30,23 +48,27 @@ const Footer = ({ lang }) => {
         <ul className="footer-nav">
           <FooterNavLink
             to={routes.seekingRoute()}
-            linkText="Seeking Services"
+            linkText={fillText(staticText.SeekingText)}
           />
           <FooterNavLink
             to={routes.offeringRoute()}
-            linkText="Offering Services"
+            linkText={fillText(staticText.SeekingText)}
           />
           <FooterNavLink
             to={routes.whyRoute()}
-            linkText="Why Does This Exist?"
+            linkText={fillText(staticText.WhyText)}
           />
-          <FooterNavLink to={routes.aboutRoute()} linkText="About Us" />
-          <FooterNavLink to={routes.resourcesRoute()} linkText="Resources" />
+          <FooterNavLink
+            to={routes.aboutRoute()}
+            linkText={fillText(staticText.AboutText)}
+          />
+          <FooterNavLink
+            to={routes.resourcesRoute()}
+            linkText={fillText(staticText.ResourcesText)}
+          />
         </ul>
       </div>
-      <div className="copyright">
-        Copyright © CURE Concordia 2019, Inc. All rights reserved.
-      </div>
+      <div className="copyright">{fillText(staticText.CopyrightText)}</div>
     </footer>
   );
 };
