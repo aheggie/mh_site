b24e9f4af13972a18f692c045749ae3a61675c64
Alex <26582461+aheggie@users.noreply.github.com>
2020-09-17 03:29:11 -0400

    OfferPage all language in bilingually!!! also link added!!

--- src/js/components/OfferingPage.js
+++ src/js/components/OfferingPage.js
@@ -1,56 +1,69 @@
 import React from "react";
 import HeaderFooter from "./HeaderFooter";
 import parse from "html-react-parser";
+import routes from "../routes";
+
+const { seekingRoute } = routes;
 
 import {
   appendTitle,
   curriedFillInStaticTextByLang,
   enAndFrContent,
 } from "../utilities";
+import { Link } from "react-router-dom";
 
 const staticText = {
-  offeringPageTitle: enAndFrContent(
-    "Offer professional mental health services",
-    "Offer professional mental health services"
+  offeringPageTitle: enAndFrContent("Offering services", "Offrez vos services"),
+  offeringContentTitle: enAndFrContent(
+    "Offer mental health support services",
+    "Offrez vos services de soutien en santé mentale"
+  ),
+  explanationLinkNotClickableText: enAndFrContent(
+    "Looking to find mental health support for yourself or others?",
+    "Cherchez vous du soutien pour votre santé mentale ou celle d’une autre personne?"
+  ),
+  explanationLinkClickableText: enAndFrContent(
+    "Go here instead.",
+    "Cliquez ici."
   ),
   offeringMainText: enAndFrContent(
     //fully not sure if this should have the <p> tag embedded
     `<p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-      malesuada tortor nisi, id mollis ligula sollicitudin dapibus.
-      Aliquam in sollicitudin magna. Vivamus vel aliquet metus.
-      Vestibulum justo arcu, pretium a vulputate eget, dapibus vitae
-      risus. Sed imperdiet erat a fringilla aliquam. Etiam at accumsan
-      tortor. Donec nulla diam, molestie nec sollicitudin eget, eleifend
-      nec libero.
+    Are you a mental health practitioner of colour working in Quebec and looking to offer your services to other people of colour? Sign up here! By joining our list, you are supporting the mental health needs of many people by increasing their access to safer therapeutic spaces. Please fill in the form below and make sure you share with your colleagues! Thank you for being part of this movement!
+    </p>
+    <p>
+    Please note that the form should take approximately 15 minutes of your time.    
     </p>`,
     `<p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-      malesuada tortor nisi, id mollis ligula sollicitudin dapibus.
-      Aliquam in sollicitudin magna. Vivamus vel aliquet metus.
-      Vestibulum justo arcu, pretium a vulputate eget, dapibus vitae
-      risus. Sed imperdiet erat a fringilla aliquam. Etiam at accumsan
-      tortor. Donec nulla diam, molestie nec sollicitudin eget, eleifend
-      nec libero.
+    Êtes-vous un professionnel de la santé mentale racisé travaillant au Québec? Désirez vous offrir vos services à d'autres personnes racisées? Inscrivez-vous ici! En vous inscrivant à notre liste, vous répondez aux besoins de santé mentale de nombreuses personnes en facilitant leur accès à des espaces thérapeutiques plus sécuritaires. Veuillez remplir le formulaire ci-dessous et assurez-vous de le partager avec vos collègues! Merci pour votre participation à ce mouvement!
+    </p>
+    <p>
+    Veuillez noter que le formulaire devrait prendre environ 15 minutes de votre temps.    
     </p>`
   ),
 };
 
 const OfferingPage = ({ match }) => {
   const lang = match.params.lang || "fr";
-  const fillInText = curriedFillInStaticTextByLang(lang);
-  appendTitle(fillInText(staticText.offeringPageTitle));
+  const fillText = curriedFillInStaticTextByLang(lang);
+  appendTitle(fillText(staticText.offeringPageTitle));
   return (
     <HeaderFooter lang={lang} className="services">
       <article className="content">
         <div className="offering inner">
           <div className="page-intro">
             <h1 className="page-title">
-              {fillInText(staticText.offeringPageTitle)}
+              {fillText(staticText.offeringContentTitle)}
             </h1>
             {/* parse() is needed because of the <p> tag in the multiline
              string above; not sure its the best approach */}
-            {parse(fillInText(staticText.offeringMainText))}
+            <p>
+              {fillText(staticText.explanationLinkNotClickableText)}{" "}
+              <Link to={seekingRoute()}>
+                {fillText(staticText.explanationLinkClickableText)}
+              </Link>
+            </p>
+            {parse(fillText(staticText.offeringMainText))}
           </div>
           {/* these buttons don't chnge with language switch */}
           <a href="" className="button purple clear-style" id="eng">
