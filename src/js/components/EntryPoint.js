f1464ad82618ec96b3353ed3eb8bbead427acbc6
Alex <26582461+aheggie@users.noreply.github.com>
2020-09-19 03:37:35 -0400

    pages load in both languages

--- src/js/components/EntryPoint.js
+++ src/js/components/EntryPoint.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 import HomePage from "./HomePage";
 import SeekingPage from "./SeekingPage/SeekingPage";
@@ -20,21 +20,23 @@ import {
   whyRoute,
 } from "../routes";
 
+const RedirectToFr = () => <Redirect to={"/fr"} />;
+
 const EntryPoint = () => {
   return (
     <Switch>
-      {/* <Route exact path="/" component={HomePage} /> */}
-      <Route exact path={homeRoute()} component={HomePage} />
+      <Route exact path="/" component={RedirectToFr} />
+      <Route exact path={homeRoute(":lang")} component={HomePage} />
       {/* <Route exact path="/seeking-services" component={SeekingPage} /> */}
-      <Route exact path={seekingRoute()} component={SeekingPage} />
+      <Route exact path={seekingRoute(":lang")} component={SeekingPage} />
       {/* <Route exact path="/offering-services" component={OfferingPage} /> */}
-      <Route exact path={offeringRoute()} component={OfferingPage} />
+      <Route exact path={offeringRoute(":lang")} component={OfferingPage} />
       {/* <Route exact path="/why" component={WhyPage} /> */}
-      <Route exact path={whyRoute()} component={WhyPage} />
+      <Route exact path={whyRoute(":lang")} component={WhyPage} />
       {/* <Route exact path="/about" component={AboutPage} /> */}
-      <Route exact path={aboutRoute()} component={AboutPage} />
+      <Route exact path={aboutRoute(":lang")} component={AboutPage} />
       {/* <Route exact path="/resources" component={ResourcesPage} /> */}
-      <Route exact path={resourcesRoute()} component={ResourcesPage} />
+      <Route exact path={resourcesRoute(":lang")} component={ResourcesPage} />
       {/* these are the dynamic routes for now */}
       {/* need to figure out how to fail effectively */}
       <Route exact path={filterRoute(":providerType")} component={FilterPage} />
--- src/js/routes.js
+++ src/js/routes.js
@@ -1,12 +1,13 @@
 //This file exists to have a single source of truth for the strings representing all static routes in the site
-const homeRoute = () => `/`;
-const seekingRoute = () => `/seeking-services`;
-const offeringRoute = () => `/offering-services`;
-const whyRoute = () => `/why`;
-const aboutRoute = () => `/about`;
-const resourcesRoute = () => `/resources`;
-const filterRoute = (profession) => `/filter-by/${profession}`;
-const practitionerRoute = (practitionerId) => `/practitioner/${practitionerId}`;
+const homeRoute = (lang) => `/${lang}/`;
+const seekingRoute = (lang) => `/${lang}/seeking-services`;
+const offeringRoute = (lang) => `/${lang}/offering-services`;
+const whyRoute = (lang) => `/${lang}/why`;
+const aboutRoute = (lang) => `/${lang}/about`;
+const resourcesRoute = (lang) => `/${lang}/resources`;
+const filterRoute = (lang, profession) => `/${lang}/filter-by/${profession}`;
+const practitionerRoute = (lang, practitionerId) =>
+  `/${lang}/practitioner/${practitionerId}`;
 
 export {
   homeRoute,
