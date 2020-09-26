551c2dfb34a480fda13dda72f4ddafe22e2451ed
Alex <26582461+aheggie@users.noreply.github.com>
2020-09-26 02:58:15 -0400

    updated MobileNav to use context hooks

--- src/js/components/Nav/MobileNav.js
+++ src/js/components/Nav/MobileNav.js
@@ -1,19 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import { MobileNavContext } from "../../providers/MobileNavActiveStateProvider";
 import NavLinks from "./NavLinks";
 
 function MobileNav({ url, lang }) {
+  const { isActive, toggleActive } = useContext(MobileNavContext);
   return (
-    <MobileNavContext.Consumer>
-      {({ isActive, toggleActive }) => (
-        <nav
-          className={isActive ? "mobile-nav mobile-nav-active" : "mobile-nav"}
-        >
-          <i className="fas fa-bars" onClick={toggleActive}></i>
-          <NavLinks url={url} lang={lang} id={"mobile-nav-links"} />
-        </nav>
-      )}
-    </MobileNavContext.Consumer>
+    <nav className={isActive ? "mobile-nav mobile-nav-active" : "mobile-nav"}>
+      <i className="fas fa-bars" onClick={toggleActive}></i>
+      <NavLinks url={url} lang={lang} id={"mobile-nav-links"} />
+    </nav>
   );
 }
 
