9469ff599b1464821694d1831277b404a6190eb6
Alex <26582461+aheggie@users.noreply.github.com>
2020-09-26 02:52:57 -0400

    DROPDOWN PERSISTS THROUGH CONTEXT API!!!!! 🎇🎇🎇🎆🎆🎆🎇🎇🎇🎆🎆🎆🎇🎇🎇🎆🎆🎆🎉🎉🎉

--- src/js/components/Nav/MobileNav.js
+++ src/js/components/Nav/MobileNav.js
@@ -1,14 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
+import { MobileNavContext } from "../../providers/MobileNavActiveStateProvider";
 import NavLinks from "./NavLinks";
 
 function MobileNav({ url, lang }) {
-  const [isActive, setIsActive] = useState(false);
-  const toggleActive = () => setIsActive(!isActive);
   return (
-    <nav className={isActive ? "mobile-nav mobile-nav-active" : "mobile-nav"}>
-      <i className="fas fa-bars" onClick={toggleActive}></i>
-      <NavLinks url={url} lang={lang} id={"mobile-nav-links"} />
-    </nav>
+    <MobileNavContext.Consumer>
+      {({ isActive, toggleActive }) => (
+        <nav
+          className={isActive ? "mobile-nav mobile-nav-active" : "mobile-nav"}
+        >
+          <i className="fas fa-bars" onClick={toggleActive}></i>
+          <NavLinks url={url} lang={lang} id={"mobile-nav-links"} />
+        </nav>
+      )}
+    </MobileNavContext.Consumer>
   );
 }
 
