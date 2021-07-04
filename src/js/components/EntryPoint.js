3b0bd83cb8cd1f2b10adf845abdef94f0af94b37
Alexandra Heggie <code@alexheggie.space>
2021-07-04 16:46:48 -0400

    fixed 404 bug with a regex validation in the routing!! added a comment explaining the langRegex fix to language routing, tested it locally and deployed and the bug fix works!!

--- src/js/components/EntryPoint.js
+++ src/js/components/EntryPoint.js
@@ -26,26 +26,32 @@ const PageNotFound = () => {
   return <h4>404444 ... Page not found</h4>;
 };
 
+//the :lang becomes a .lang prop on the match object router sends along
+//the (en|fr) is and added regex that validtes if it is one of our valid languages
+//to add another language would require an update to this string (amongst many other code changes)
+//i.e. adding spanish might result in ":lang(en|fr|sp)"
+const langRegex = ":lang(en|fr)";
+
 const EntryPoint = () => {
   return (
     <Switch>
       <Route exact path="/" component={RedirectToFr} />
-      <Route exact path={homeRoute(":lang")} component={HomePage} />
-      <Route exact path={seekingRoute(":lang")} component={SeekingPage} />
-      <Route exact path={offeringRoute(":lang")} component={OfferingPage} />
-      <Route exact path={whyRoute(":lang")} component={WhyPage} />
-      <Route exact path={aboutRoute(":lang")} component={AboutPage} />
-      <Route exact path={resourcesRoute(":lang")} component={ResourcesPage} />
+      <Route exact path={homeRoute(langRegex)} component={HomePage} />
+      <Route exact path={seekingRoute(langRegex)} component={SeekingPage} />
+      <Route exact path={offeringRoute(langRegex)} component={OfferingPage} />
+      <Route exact path={whyRoute(langRegex)} component={WhyPage} />
+      <Route exact path={aboutRoute(langRegex)} component={AboutPage} />
+      <Route exact path={resourcesRoute(langRegex)} component={ResourcesPage} />
       {/* these are the dynamic routes for now */}
       {/* need to figure out how to fail effectively */}
       <Route
         exact
-        path={filterRoute(":lang", ":providerType")}
+        path={filterRoute(langRegex, ":providerType")}
         component={FilterPage}
       />
       <Route
         exact
-        path={practitionerRoute(":lang", ":practitionerId")}
+        path={practitionerRoute(langRegex, ":practitionerId")}
         component={PractitionerPage}
       />
       {/* this will match ALL incoming routes and needs to ALWAYS be the last route in the switch */}
